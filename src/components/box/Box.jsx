78e66a662e7476eb682179ee876e0f650ebb8e7c
faisalrehman <faisalfasi18@gmail.com>
2023-06-05 11:32:30 +0200

    basic gameplay completed with updated bg

--- src/components/box/Box.jsx
+++ src/components/box/Box.jsx
@@ -8,7 +8,7 @@ const Box = (props) => {
       {!props.isClicked ? (
         <div className="w-16 h-16 bg-blue-500 m-2" onClick={handleClick}></div>
       ) : (
-        <div className="w-16 h-16  m-2">
+        <div className="w-16 h-16 bg-blue-300 m-2">
           <img src={props.imgSrc} alt="" className=" w-16 h-16" />
           {/* {props.value} */}
         </div>
